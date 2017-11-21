75fe021c6c4b3d038c0da51bc7794777f93fdd1b
yasukun <masa.yasu.kun@gmail.com>
2017-11-21 11:07:23 +0900

    update

--- problem_4.rs
+++ problem_4.rs
@@ -1,4 +1,4 @@
-let threedigits = (100..1000).collect::<Vec<u32>>();
+let threedigits = (100..1000).rev().collect::<Vec<u32>>();
 
 fn inversion(num: u32) -> u32 {
     num.to_string().chars().rev().collect::<String>().parse::<u32>().unwrap()
@@ -9,7 +9,7 @@ let mut maxnum =  0;
 for i in threedigits.iter() {
     for j in threedigits.iter() {
         let product = i * j;
-        if inversion(product) == product {
+        if product == inversion(product) {
             if maxnum < product {
                 maxnum = product;
             }
