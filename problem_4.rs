a2165bb493a14bea38f8605eddf9a66755415520
yasukun <masa.yasu.kun@gmail.com>
2017-11-21 10:45:53 +0900

    Create problem_4.rs

---
+++ problem_4.rs
@@ -0,0 +1,20 @@
+let threedigits = (100..999).collect::<Vec<u32>>();
+
+fn inversion(num: u32) -> u32 {
+    num.to_string().chars().rev().collect::<String>().parse::<u32>().unwrap()
+}
+
+let mut maxnum =  0;
+
+for i in threedigits.iter() {
+    for j in threedigits.iter() {
+        let product = i * j;
+        if inversion(product) == product {
+            if maxnum < product {
+                maxnum = product;
+            }
+        }
+    }
+}
+
+println!("{}", maxnum);
