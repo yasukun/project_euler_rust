9645949cfc6e2b8f26babbbecaf3dbab194d79ba
yasukun <masa.yasu.kun@gmail.com>
2017-11-22 11:46:45 +0900

    Update problem_6.rs

--- problem_6.rs
+++ problem_6.rs
@@ -1,4 +1,3 @@
-let nums = (1..101).collect::<Vec<u32>>();
-let sum_of_squares =  nums.iter().map(|n| n * n).fold(0, |sum, i| sum + i);
-let square_of_sums = nums.iter().fold(0, |sum, i| sum + i).pow(2);
+let sum_of_squares =  (1..101).map(|n| n * n).sum::<u64>();
+let square_of_sums = (1..101).sum::<u64>().pow(2);
 println!("{}", square_of_sums - sum_of_squares);
