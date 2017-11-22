0f612d2f0c56ed0f5d134d6317b11eb0b9f3d024
yasukun <masa.yasu.kun@gmail.com>
2017-11-22 11:48:39 +0900

    Update problem_6.rs

--- problem_6.rs
+++ problem_6.rs
@@ -1,3 +1,3 @@
-let sum_of_squares =  (1..101).map(|n| n * n).sum::<u64>();
-let square_of_sums = (1..101).sum::<u64>().pow(2);
+let sum_of_squares =  (1..101).map(|n| n * n).sum::<u32>();
+let square_of_sums = (1..101).sum::<u32>().pow(2);
 println!("{}", square_of_sums - sum_of_squares);
