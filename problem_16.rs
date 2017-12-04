9a86cb19fcf4ecda9c18278e5d155154cd8ee7bc
yasukun <masa.yasu.kun@gmail.com>
2017-12-04 14:02:07 +0900

    Create problem_16.rs

---
+++ problem_16.rs
@@ -0,0 +1,7 @@
+extern crate num;
+
+use num::bigint::{BigInt, ToBigInt};
+use std::iter;
+
+let target = iter::repeat(2).take(1000).map(|c| c.to_bigint().unwrap()).fold(1.to_bigint().unwrap(), |sum, i| sum.checked_mul(&i).unwrap());
+println!("{:?}", target.to_string().chars().map(|c| c.to_string().parse::<u32>().unwrap()).sum::<u32>());
